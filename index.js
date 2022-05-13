a09d933c0131ac4ec2acf52e33d6d80b8133462a
Anshul Choudhary <ansh121@github.com>
2022-05-13 15:51:54 +0530

    error fix

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-async function run() {
-    try{
-        const token = core.getInput('token');
-        const octokit = github.getOctokit(token);
-        const context = github.context;
+try{
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+    const context = github.context;
 
-        const list = await octokit.paginate(octokit.rest.issues.listForRepo, {
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            state: 'all',
-          });
+    const list = await octokit.paginate(octokit.rest.issues.listForRepo, {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        state: 'all',
+        });
 
-        const issue_stats = { open: 0, closed: 0 }
-        const pull_request_stats = { open: 0, closed: 0, merged: 0 }
+    const issue_stats = { open: 0, closed: 0 }
+    const pull_request_stats = { open: 0, closed: 0, merged: 0 }
 
-            for(const item in list){
-            if('pull_request' in item){
-                if(item.state == 'open') pull_request_stats.open++;
-                else if(item.merged_at == null) pull_request_stats.closed++;
-                else pull_request_stats.merged++;
-            }
-            else{
-                if(item.state == 'open') issue_stats.open++;
-                else issue_stats.closed++;
-            }
+        for(const item in list){
+        if('pull_request' in item){
+            if(item.state == 'open') pull_request_stats.open++;
+            else if(item.merged_at == null) pull_request_stats.closed++;
+            else pull_request_stats.merged++;
         }
-
-        core.setOutput('pull_request_stats', pull_stats);
-        core.setOutput('issue_stats', issue_stats)
-    } catch (error) {
-        core.setFailed(error.message);
+        else{
+            if(item.state == 'open') issue_stats.open++;
+            else issue_stats.closed++;
+        }
     }
-}
 
-run();
+    core.setOutput('pull_request_stats', pull_stats);
+    core.setOutput('issue_stats', issue_stats)
+} catch (error) {
+    core.setFailed(error.message);
+}
